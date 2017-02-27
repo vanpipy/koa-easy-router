70cf30051611ee247146362ca795a14ece29c258
vanpipy <vanpipy@gmail.com>
2017-02-27 17:57:51 +0800

    Implement a basic router and idea come from http.createServer.

--- index.js
+++ index.js
@@ -10,16 +10,39 @@ const methods = [
     'DELETE'
 ];
 
-function Router () {
+function Router() {
+    let _Router = this;
+
+    this.stacks = [];
+    this.methods = methods;
+
+    methods.map((method) => {
+        _Router[method.toLowerCase()] = registerGenerator(method, _Router);
+    });
+
     return this;
 }
 
-Router.prototype.route = () => {
+Router.prototype.route = function () {
+    const _Router = this;
 
-    return (ctx, next) => {
+    const dispatch = (ctx, next) => {
+        let i = _Router.stacks.length;
+        while (i--) {
+            const stack = _Router.stacks[i];
 
+            if (stack.method === ctx.req.method && stack.path === ctx.req.url) {
+                return next().then(stack.func(ctx, next));
+            }
+        }
     };
+
+    return dispatch;
 };
+
+function registerGenerator (method, Router) {
+    return (path, func) => (Router.stacks.push({ path, method, func }));
+}
 
 //TODO: nested route.
 
